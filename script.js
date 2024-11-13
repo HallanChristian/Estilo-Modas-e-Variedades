cfc9bd33ccb7cca3174c3c992a9e5fd0279398de
Hallan Christian <hallanchristiancruz@gmail.com>
2024-11-12 22:51:54 -0300

    Melhorias adicionais

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-    function toggleMenu() {
-        const mobileMenu = document.getElementById('mobileMenu');
-        mobileMenu.classList.toggle('show');
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    mobileMenu.classList.toggle('show');
+    hamburgerMenu.classList.toggle('open');
+}
+
+document.addEventListener('click', function(event) {
+    const mobileMenu = document.getElementById('mobileMenu');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    if (!mobileMenu.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+        mobileMenu.classList.remove('show');
+        hamburgerMenu.classList.remove('open');
     }
+});
 
